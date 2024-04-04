5bf718f821e3fa8bb7b5859f10e11786dde1c516
vnadoda <vipul@carapace.finance>
2024-04-04 15:28:27 -0500

    Removed test code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,13 @@
 "use client";
 import React, { useState } from "react";
-import { getTokensOwnedByAddress } from "./service/tokenService";
 
 export default function Home() {
-  const [value, setValue] = useState<string>("");
+  const [value, setValue] = useState<string>(
+    "0xd8da6bf26964af9d7eed9e03e53415d37aa96045"
+  );
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setValue(event.target.value);
   };
-
-  // Test code
-  const account = "0xd8da6bf26964af9d7eed9e03e53415d37aa96045";
-  let tokens: any[] = [];
-  // tokens = await getTokensOwnedByAddress(account);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -24,19 +20,6 @@ export default function Home() {
         you.
       </p>
       <input type="text" value={value} onChange={handleChange} />
-
-      <p className="text-2xl font-bold mb-5 ">
-        Tokens owned by address {account}
-      </p>
-      <div>
-        {tokens.map((token: any) => (
-          <div key={token.name} className="flex items-center justify-between">
-            <div>{token.name}</div>
-            <div>{token.balance}</div>
-            <div>{token.symbol}</div>
-          </div>
-        ))}
-      </div>
     </main>
   );
 }
