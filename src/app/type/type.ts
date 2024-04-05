cd1a0a4eccdb84949cb22767c633bfbed8dc083f
Tai <4382091+taisukemino@users.noreply.github.com>
2024-04-05 12:22:43 -0700

    change types

--- src/app/type/type.ts
+++ src/app/type/type.ts
@@ -14,18 +14,22 @@ export interface DebtPosition {
   LTV: number; // debtAmount / collateralAmount
 }
 
+export interface MorphoBlueDebtPosition extends DebtPosition {
+  marketId: string; // This is required because there can be two markets with the same collateral token and a debt token.
+}
+
 export interface Market {
   Trailing30DaysBorrowingAPY: number; // borrowing cost of a debt token or an underling asset.
   maxLTV: number; // the maximum LTV in this market.
 }
 
-export interface MorphoBlue extends Market {
+export interface MorphoBlueMarket extends Market {
   marketId: string; // This is required because two markets with the same collateral token and a debt token can have different borrowing APY and maxLTV.
   collateralToken: Token;
   debtToken: Token;
 }
 
-export interface AaveV3 extends Market {
+export interface AaveV3Market extends Market {
   underlyingAsset: Token;
   Trailing30DaysLendingAPY: number;
 }
