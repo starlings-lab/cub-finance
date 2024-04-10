839df3774f41aa533fb73627ed4c87b4d40b0a66
Tai <4382091+taisukemino@users.noreply.github.com>
2024-04-09 19:50:09 -0700

    ERC20 tokens constant

---
+++ src/app/contracts/ERC20Tokens.ts
@@ -0,0 +1,57 @@
+import { Token } from "../type/type";
+
+export const USDC: Token = {
+  address: `0x${"A0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"}`,
+  name: "USD Coin",
+  decimals: 6,
+  symbol: "USDC"
+};
+
+export const WETH: Token = {
+  address: `0x${"C02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"}`,
+  name: "Wrapped Ether",
+  decimals: 18,
+  symbol: "WETH"
+};
+
+export const COMP: Token = {
+  address: `0x${"c00e94Cb662C3520282E6f5717214004A7f26888"}`,
+  name: "Compound",
+  decimals: 18,
+  symbol: "COMP"
+};
+
+export const WBTC: Token = {
+  address: `0x${"2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599"}`,
+  name: "Wrapped BTC",
+  decimals: 8,
+  symbol: "WBTC"
+};
+
+export const UNI: Token = {
+  address: `0x${"1f9840a85d5aF5bf1D1762F925BDADdC4201F984"}`,
+  name: "Uniswap",
+  decimals: 18,
+  symbol: "UNI"
+};
+
+export const LINK: Token = {
+  address: `0x${"514910771AF9Ca656af840dff83E8264EcF986CA"}`,
+  name: "ChainLink Token",
+  decimals: 18,
+  symbol: "LINK"
+};
+
+export const cbETH: Token = {
+  address: `0x${"Be9895146f7AF43049ca1c1AE358B0541Ea49704"}`,
+  name: "Coinbase Wrapped Staked ETH",
+  decimals: 18,
+  symbol: "cbETH"
+};
+
+export const wstETH: Token = {
+  address: `0x${"7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0"}`,
+  name: "Wrapped liquid staked Ether 2.0",
+  decimals: 18,
+  symbol: "wstETH"
+};
