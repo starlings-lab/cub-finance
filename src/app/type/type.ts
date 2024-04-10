f50f24d3e09dbabe6e37e916ab5c3c51d3ec7489
Tai <4382091+taisukemino@users.noreply.github.com>
2024-04-10 09:06:33 -0700

    change types

--- src/app/type/type.ts
+++ src/app/type/type.ts
@@ -12,51 +12,68 @@ export interface Token {
   symbol: string;
 }
 
+export interface TokenAmount {
+  token: Token;
+  amount: BigInt;
+  amountInUSD: number;
+}
+
 export interface DebtPositionTableRow {
-  protocol: string;
-  debtToken: Token;
+  protocol: Protocol;
+  debtToken: Token[];
   collateralTokens: Token[];
-  debtAmount: BigInt;
-  collateralAmount: BigInt;
-  LTV: number; // debtAmount / collateralAmount
+  totalCollateralAmountInUSD: number;
+  totalDebtAmountInUSD: number;
+  LTV: number; // debtAmountInUSD / sum of collateralAmountInUSD array
   maxLTV: number;
+  Trailing30DaysNetAPY: number;
+  Trailing30DaysLendingAPY: number;
   Trailing30DaysBorrowingAPY: number;
 }
 
+export interface UserDebtDetailsBase {
+  protocol: Protocol;
+  debtPositions: DebtPosition[];
+}
+
+export interface MorphoBlueUserDebtDetails extends UserDebtDetailsBase {
+  markets: MorphoBlueMarket[];
+}
+
+export interface UserDebtDetails extends UserDebtDetailsBase {
+  markets: Market[];
+}
+
 export interface DebtPosition {
-  collateralTokens: Token[];
-  debtToken: Token;
-  collateralAmount: BigInt;
-  debtAmount: BigInt;
-  LTV: number; // debtAmount / collateralAmount
+  debt: TokenAmount[];
+  collateral: TokenAmount[];
+  LTV: number; // debtAmountInUSD / sum of collateralAmountInUSD array
 }
 
 export interface MorphoBlueDebtPosition extends DebtPosition {
   marketId: string; // This is required because there can be two markets with the same collateral token and a debt token.
 }
 
-export interface Market {
-  Trailing30DaysBorrowingAPY: number; // borrowing cost of a debt token or an underling asset.
+export interface MarketBase {
   maxLTV: number; // the maximum LTV in this market.
+  Trailing30DaysLendingAPY: number;
+  Trailing30DaysBorrowingAPY: number; // borrowing cost of a debt token or an underling asset.
 }
 
-export interface MorphoBlueMarket extends Market {
+export interface MorphoBlueMarket extends MarketBase {
   marketId: string; // This is required because two markets with the same collateral token and a debt token can have different borrowing APY and maxLTV.
   collateralToken: Token;
   debtToken: Token;
 }
 
-export interface AaveV3Market extends Market {
+export interface Market extends MarketBase {
   underlyingAsset: Token;
   Trailing30DaysLendingAPY: number;
 }
 
-export interface Protocol {
-  name: string;
-  supportedDebtTokens: Token[];
-  supportedCollateralTokens: Token[];
-}
-
-export interface ILendingMarketService {
-  getProtocol(): Promise<Protocol>;
+export enum Protocol {
+  AaveV3 = "AaveV3",
+  CompoundV3 = "CompoundV3",
+  MorphoBlue = "MorphoBlue",
+  Spark = "Spark"
 }
