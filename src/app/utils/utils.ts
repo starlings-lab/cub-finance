57d8dab0326691e76dc80854bedc33b2cf22c6f4
Tai <4382091+taisukemino@users.noreply.github.com>
2024-04-11 16:37:44 -0700

    stop returning undefined

--- src/app/utils/utils.ts
+++ src/app/utils/utils.ts
@@ -11,9 +11,13 @@ import {
 } from "../contracts/ERC20Tokens";
 import { ethers } from "ethers";
 
-export function getTokenByAddress(address: string): Token | undefined {
+export function getTokenByAddress(address: string): Token {
   const tokens: Token[] = [USDC, WETH, COMP, WBTC, UNI, LINK, cbETH, wstETH];
-  return tokens.find(
+  const foundToken = tokens.find(
     (token) => ethers.getAddress(token.address) === ethers.getAddress(address)
   );
+  if (!foundToken) {
+    throw new Error(`Token not found for address: ${address}`);
+  }
+  return foundToken;
 }
