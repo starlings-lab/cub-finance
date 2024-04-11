60f9b80d4d64acb255f792035698854f8593f261
Tai <4382091+taisukemino@users.noreply.github.com>
2024-04-11 16:37:23 -0700

    constant values for the compound v3 service

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,6 +1,6 @@
 export const ALCHEMY_API_URL = `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY_ETH_MAINNET}`;
+export const ALCHEMY_API_URL_2 = `https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY_ETH_MAINNET}`;
 export const ALCHEMY_RPC_URL = `https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY_ETH_MAINNET}`;
 export const MESSARI_GRAPHQL_URL =
   "https://api.thegraph.com/subgraphs/name/messari/aave-v3-ethereum";
-
 export const MORPHO_GRAPHQL_URL = "https://blue-api.morpho.org/graphql";
