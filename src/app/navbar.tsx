24bdfba64613c1e1c8acb536859c1ceeb317be5b
Asif Ali Khan <asifkhan0410@gmail.com>
2024-04-21 15:44:32 +0530

    feat: search bar component extraction

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import Image from "next/image";
+import { SearchBar } from "@/components/ui/search-bar";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
@@ -16,29 +15,8 @@ export default function Navbar() {
               <p className="nav-home text-2xl text-[#F43F5E] font-bold">ReFi</p>
             </Link>
           </div>
-          {/* TODO: add logic here for getting the data */}
           {pathname.includes("user") && (
-            <div className="flex w-full items-center justify-center space-x-2">
-              <div className="flex w-full max-w-xl space-x-2 py-1 pl-3 pr-1 border rounded-3xl">
-                <Image
-                  src={"/search_black.svg"}
-                  alt="icon"
-                  width="32"
-                  height="32"
-                />
-                <Input
-                  // ref={inputRef}
-                  className="placeholder:text-slate-400"
-                  type="text"
-                  // value={value}
-                  placeholder="Enter your wallet address"
-                  // onChange={handleChange}
-                ></Input>
-                {/* <Button className="bg-[#F43F5E] text-white rounded-3xl w-36">
-                <Link href={`/user`}>Find Now</Link>
-              </Button> */}
-              </div>
-            </div>
+            <SearchBar isHome={false} defaultUserAddress=""/>
           )}
           <div className="mt-auto mb-auto">
             <Button className="bg-[#F43F5E3a] hover:bg-[#F43F5E] text-black hover:text-white rounded-3xl w-36">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,6 @@
 "use client";
-import React, { useEffect, useRef, useState } from "react";
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
+import React from "react";
 
-import { getCompoundV3UserDebtDetails } from "./service/compoundV3Service";
-import { getRecommendedDebtDetail } from "./service/aaveV3Service";
-import { Protocol } from "./type/type";
-import { getRecommendations } from "./service/refiananceRecommendationService";
 import {
   Card,
   CardContent,
@@ -14,37 +8,13 @@ import {
   CardHeader,
   CardTitle
 } from "@/components/ui/card";
-import Link from "next/link";
-import Image from "next/image";
+import { SearchBar } from "@/components/ui/search-bar";
 
 export default function Home() {
   // TODO: remove test users
   const aaveUser = "0x00171ab2f44c1c9b21c7696eb1a5c601f05a9167";
   const compoundUser1 = "0xfe99cc4664a939f826dbeb545c1aad4c89ee737a";
   const compoundUser2 = "0x9CF423E929d661a0fB25e4AEf05bEB1037298fFb";
-
-  const [value, setValue] = useState<string>(aaveUser);
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setValue(event.target.value);
-  };
-
-  const inputRef = useRef<HTMLInputElement>(null);
-
-  useEffect(() => {
-    const handleKeyDown = (event: KeyboardEvent) => {
-      if (event.metaKey && event.key === "k") {
-        // Check for Cmd + K
-        event.preventDefault(); // Prevent default behavior (like browser search)
-        inputRef.current?.focus(); // Focus on the input box
-      }
-    };
-
-    document.addEventListener("keydown", handleKeyDown);
-
-    return () => {
-      document.removeEventListener("keydown", handleKeyDown);
-    };
-  }, []); // Run this effect only once on component mount
 
   // const allRecommendations = await getRecommendations(
   //   Protocol.CompoundV3,
@@ -68,27 +38,11 @@ export default function Home() {
             ReFi analyzes your existing debt positions and find better terms for
             you.
           </CardDescription>
-          <div className="flex w-full items-center justify-center space-x-2">
-            <div className="flex w-full max-w-xl space-x-2 mt-16 py-1 pl-3 pr-1 border rounded-3xl">
-              <Image
-                src={"/search_black.svg"}
-                alt="icon"
-                width="32"
-                height="32"
-              />
-              <Input
-                ref={inputRef}
-                className="placeholder:text-slate-400"
-                type="text"
-                value={value}
-                placeholder="Enter your wallet address"
-                onChange={handleChange}
-              ></Input>
-              <Button className="bg-[#F43F5E] text-white rounded-3xl w-36">
-                <Link href={`/user/${value}`}>Find Now</Link>
-              </Button>
-            </div>
-          </div>
+          <SearchBar
+            className="mt-16"
+            isHome={true}
+            defaultUserAddress={aaveUser}
+          />
         </CardContent>
       </Card>
     </div>
