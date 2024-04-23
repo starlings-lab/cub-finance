f03647712958241f12b5bdf4abaf01475751eabb
Tai <4382091+taisukemino@users.noreply.github.com>
2024-04-23 13:52:09 -0700

    improve getTokenByAddress

--- src/app/utils/utils.ts
+++ src/app/utils/utils.ts
@@ -1,6 +1,13 @@
+import { Address } from "abitype";
 import { Token } from "../type/type";
 import {
   USDC,
+  DAI,
+  USDT,
+  PYUSD,
+  USDA,
+  LUSD,
+  crvUSD,
   WETH,
   COMP,
   WBTC,
@@ -11,11 +18,26 @@ import {
 } from "../contracts/ERC20Tokens";
 import { ethers } from "ethers";
 
-export function getTokenByAddress(address: string): Token {
-  const tokens: Token[] = [USDC, WETH, COMP, WBTC, UNI, LINK, cbETH, wstETH];
-  const foundToken = tokens.find(
-    (token) => ethers.getAddress(token.address) === ethers.getAddress(address)
-  );
+export function getTokenByAddress(address: string | Address): Token {
+  const tokens: Token[] = [
+    USDC,
+    DAI,
+    USDT,
+    PYUSD,
+    USDA,
+    LUSD,
+    crvUSD,
+    WETH,
+    COMP,
+    WBTC,
+    UNI,
+    LINK,
+    cbETH,
+    wstETH
+  ];
+  const foundToken = tokens.find((token) => {
+    return ethers.getAddress(token.address) === ethers.getAddress(address);
+  });
   if (!foundToken) {
     throw new Error(`Token not found for address: ${address}`);
   }
