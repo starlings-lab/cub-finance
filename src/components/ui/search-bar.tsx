045aa02d65b26ea125cf93a53b9c014e82f53661
Asif Ali Khan <asifkhan0410@gmail.com>
2024-04-25 00:47:44 +0530

    feat: added error for bad address input

--- src/components/ui/search-bar.tsx
+++ src/components/ui/search-bar.tsx
@@ -6,6 +6,7 @@ import { Button } from "./button";
 import Link from "next/link";
 import { cn } from "@/lib/utils";
 import { useRouter } from "next/navigation";
+import { isAddress } from "ethers";
 
 export interface SearchBarProps
   extends React.InputHTMLAttributes<HTMLInputElement> {
@@ -17,7 +18,10 @@ const SearchBar = React.forwardRef<HTMLInputElement, SearchBarProps>(
   ({ className, type, defaultUserAddress, isHome, ...props }, ref) => {
     const router = useRouter();
     const [value, setValue] = React.useState<string>(defaultUserAddress);
+    const [addressErr, setAddressErr] = React.useState<boolean>(false);
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+      const isValidAddress = isAddress(event.target.value);
+      setAddressErr(!isValidAddress);
       setValue(event.target.value);
     };
 
@@ -47,28 +51,48 @@ const SearchBar = React.forwardRef<HTMLInputElement, SearchBarProps>(
           className
         )}
       >
-        <div className="hidden sm:flex w-full max-w-xl space-x-2 py-1 pl-3 pr-1 border rounded-3xl">
-          <Image src={"/search_black.svg"} alt="icon" width="32" height="32" />
-          <Input
-            ref={inputRef}
-            className="placeholder:text-slate-400"
-            type="text"
-            value={value}
-            placeholder="Enter your wallet address"
-            onChange={handleChange}
-            onBlur={() => !isHome && value && router.push(`/user/${value}`)}
-          ></Input>
-          <Button
-            disabled={!isHome}
-            className={`bg-[#F43F5E] text-white rounded-3xl w-36 ${
-              !isHome && "disabled:opacity-0"
-            }`}
+        <div className="hidden sm:flex w-full max-w-xl">
+          <div
+            className={`space-x-2 py-1 pl-3 pr-1 border ${
+              addressErr ? "border-red-500" : ""
+            } rounded-3xl`}
           >
-            <Link href={`/user/${value}`}>Find Now</Link>
-          </Button>
+            <Image
+              src={"/search_black.svg"}
+              alt="icon"
+              width="32"
+              height="32"
+            />
+            <Input
+              ref={inputRef}
+              className="placeholder:text-slate-400"
+              type="text"
+              value={value}
+              placeholder="Enter your wallet address"
+              onChange={handleChange}
+              onBlur={() => !isHome && value && router.push(`/user/${value}`)}
+            ></Input>
+            <Button
+              disabled={!isHome || addressErr}
+              className={`bg-[#F43F5E] text-white rounded-3xl w-36 ${
+                !isHome && "disabled:opacity-0"
+              }`}
+            >
+              <Link href={`/user/${value}`}>Find Now</Link>
+            </Button>
+          </div>
+          {addressErr && (
+            <div className="text-red-500 hidden pl-3 pt-1 sm:flex text-sm">
+              Enter a valid address
+            </div>
+          )}
         </div>
         <div className="flex flex-col sm:hidden w-full">
-          <div className="flex border rounded-3xl py-1 px-3">
+          <div
+            className={`flex border ${
+              addressErr ? "border-red-500" : ""
+            } rounded-3xl py-1 px-3`}
+          >
             <Input
               ref={inputRef}
               className="placeholder:text-slate-400 rounded-3xl"
@@ -79,8 +103,13 @@ const SearchBar = React.forwardRef<HTMLInputElement, SearchBarProps>(
               onBlur={() => !isHome && value && router.push(`/user/${value}`)}
             ></Input>
           </div>
+          {addressErr && (
+            <div className="text-red-500 text-sm pl-3 pt-1">
+              Enter a valid address
+            </div>
+          )}
           <Button
-            disabled={!isHome}
+            disabled={!isHome || addressErr}
             className={`bg-[#F43F5E] text-white rounded-3xl w-full mt-2 ml-0 ${
               !isHome && "disabled:opacity-0"
             }`}
