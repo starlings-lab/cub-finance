e36f46393e5e1761ac4aa2fe75ace1492848a957
vnadoda <vipul@carapace.finance>
2024-04-25 14:40:32 -0500

    Fixed a popover component's mobile check logic

--- src/components/ui/popover.tsx
+++ src/components/ui/popover.tsx
@@ -12,14 +12,17 @@ const PopoverWrapper = ({
   content: ReactNode;
 }) => {
   const isMobile = useMemo(
-    () => /iPhone|iPad|iPod|Android/i.test(navigator.userAgent),
+    () => navigator && /iPhone|iPad|iPod|Android/i.test(navigator.userAgent),
     []
   );
 
   if (isMobile) {
     return (
       <Popover.Root>
-        <Popover.Trigger className="cursor-pointer" onClick={(e) => e.stopPropagation()}>
+        <Popover.Trigger
+          className="cursor-pointer"
+          onClick={(e) => e.stopPropagation()}
+        >
           {title}
         </Popover.Trigger>
         <Popover.Portal>
@@ -37,9 +40,7 @@ const PopoverWrapper = ({
     return (
       <Tooltip.Provider>
         <Tooltip.Root>
-          <Tooltip.Trigger className="cursor-pointer">
-            {title}
-          </Tooltip.Trigger>
+          <Tooltip.Trigger className="cursor-pointer">{title}</Tooltip.Trigger>
           <Tooltip.Portal>
             <Tooltip.Content
               className="bg-white rounded shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] select-none will-change-[transform,opacity] px-2 py-2.5"
